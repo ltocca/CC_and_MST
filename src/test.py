f2030db11ba9f601544701fdb8e3f25761d525a7
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2023-01-19 13:23:34 +0100

    prime implementazioni test (valutare)

--- src/test.py
+++ src/test.py
@@ -1,3 +1,17 @@
 from timeit import default_timer as timer
 from cc import cc
 from mst_kruskal import *
+
+
+def cc_test(g):
+    start = timer()
+    conn_comp = cc(g)
+    end = timer()
+    return [conn_comp.get_dim(), round(end - start, 6)]
+
+
+def mst_test(g):
+    start = timer()
+    mst = mst_kruskal(g)
+    end = timer()
+    return [mst, round(end - start, 6)]
