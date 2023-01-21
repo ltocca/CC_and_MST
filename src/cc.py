44c249a7e9b24b432fff1c8468606db81dca001a
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2023-01-21 15:35:39 +0100

    corretto errore implementazione algoritmo ricerca delle componenti connesse

--- src/cc.py
+++ src/cc.py
@@ -1,11 +1,11 @@
 from union_find import *
 
+
 def cc(G):
     uf = UnionFind()
-    for v in range(G.size()):
+    for v in G.v:
         uf.make_set(v)
-    for i in range(G.size):
-        for j in range (G.size):
-            if uf.find_set(i) is not uf.find_set(j):
-                uf.union(i, j)
+    for i in G.edges:
+        if uf.find_set(i[0]) is not uf.find_set(i[1]):
+            uf.union(i[0], i[1])
     return uf
