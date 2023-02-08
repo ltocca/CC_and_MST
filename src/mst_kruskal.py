6fe191a57d05c95c356a8b7f0992b54d2dee8a84
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2023-02-08 15:40:01 +0100

    fix mst_kruskal.py indici

--- src/mst_kruskal.py
+++ src/mst_kruskal.py
@@ -5,14 +5,14 @@ from union_find import *
 def mst_kruskal(G):  # in questo caso ometto matrice dei pesi, le unisco in G (non definitivo)
     A = []
     uf = UnionFind()
-    for v in range(G.size()):
+    for v in G.v:
         uf.make_set(v)
     e = G.sort_edges()
     count = 0
     for i in e:
         if uf.find_set(i[0]) is not uf.find_set(i[1]):
             A.append(i)
-            uf.union(i[1], i[2])
+            uf.union(i[0], i[1])
             count += 1
             if count is G.size()-1:
                 break
