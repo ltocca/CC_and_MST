fcec5f79d39357e86b9e1f7c9b3a5b8a4c1f3709
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2023-02-15 15:02:29 +0100

    update test

--- src/test.py
+++ src/test.py
@@ -4,13 +4,6 @@ from mst_kruskal import *
 import matplotlib.pyplot as plt
 
 
-def dfs_test(g):
-    start = timer()
-    g.dfs()
-    end = timer()
-    return round(end - start, 6)
-
-
 def cc_test(g):
     start = timer()
     conn_comp = cc(g)
@@ -26,59 +19,67 @@ def mst_test(g):
 
 
 def test():
-    dfs_results = []
     cc_results = []
+    cc_results_time = []
     mst_results = []
-    dfs_results_weighted = []
+    mst_results_time = []
     cc_results_weighted = []
+    cc_results_time_weighted = []
     mst_results_weighted = []
+    mst_results_time_weighted = []
     n = 5  # numero iniziale di nodi 5
     p = 10  # probabilità di archi iniziale 10
 
-    for i in range(1, 10):  # dovrebbe essere 13
+    for i in range(0, 10):  # dovrebbe essere 13
         g = Graph(n)
         g.populate_adj(p)
-        dfs_results.append(dfs_test(g))
-        cc_results.append(cc_test(g)[1])
-        mst_results.append(mst_test(g))
+        comp = cc_test(g)
+        cc_results.append(comp[0])
+        cc_results_time.append(comp[1])
+        mst = mst_test(g)
+        mst_results.append(mst[0])
+        mst_results_time.append(mst[1])
 
         g = Graph(n, True)
         g.populate_adj(p)
-        dfs_results_weighted.append(dfs_test(g))
-        cc_results_weighted.append(cc_test(g)[1])
-        mst_results_weighted.append(mst_test(g))
+        comp = cc_test(g)
+        cc_results_weighted.append(comp[0])
+        cc_results_time_weighted.append(comp[1])
+        mst = mst_test(g)
+        mst_results_weighted.append(mst[0])
+        mst_results_time_weighted.append(mst[1])
         p += 5
         # n += int((n * i) / 4)
         n += 5
 
-    x = np.arange(1, len(dfs_results) + 1)
+    x = np.arange(1, len(mst_results_time) + 1)*10
     plot_1 = plt.figure(1)
-    plt.plot(x, dfs_results)
-    plt.plot(x, dfs_results_weighted)
+    plt.plot(x, cc_results)
+    plt.plot(x, mst_results)
     plt.xlabel("Dimensione, probabilità")
-    plt.ylabel("Tempo in s")
-    plt.legend(["Non pesato", "Pesato"])
-    plt.title("Confronto esecuzione DFS tra grafo pesato e non pesato")
+    plt.ylabel("Numero di Mst e CC")
+    plt.legend(["cc", "mst"])
+    plt.title("Confronto esecuzione CC e MST con grafo non pesato")
 
     plot_2 = plt.figure(2)
-    plt.plot(x, cc_results)
     plt.plot(x, cc_results_weighted)
+    plt.plot(x, mst_results_weighted)
     plt.xlabel("Dimensione, probabilità")
-    plt.ylabel("Componenti connesse")
-    plt.legend(["Non pesato", "Pesato"])
-    plt.title("Confronto esecuzione CC tra grafo pesato e non pesato")
+    plt.ylabel("Numero di MST e Componenti connesse")
+    plt.legend(["CC", "MST"])
+    plt.title("Confronto esecuzione CC e MST con grafo pesato")
 
     plot_3 = plt.figure(3)
-    plt.plot(x, mst_results)
+    plt.plot(x, cc_results_time)
     plt.plot(x, mst_results_weighted)
     plt.xlabel("Dimensione, probabilità")
     plt.ylabel("Tempo in s")
-    plt.legend(["Non pesato", "Pesato"])
-    plt.title("Confronto esecuzione MST tra grafo pesato e non pesato")
+    plt.legend(["Componente connesse", "mst"])
+    plt.title("Confronto temporale esecuzione CC e MST")
 
-    plot_1.savefig('img/dfs.png')
-    plot_2.savefig('img/cc.png')
-    plot_3.savefig('img/mst.png')
+    plot_1.savefig('img/cc_mst.png')
+    plot_2.savefig('img/cc_mst_w.png')
+    plot_3.savefig('img/time.png')
     plt.clf()
 
 
