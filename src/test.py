688584a3188f77d536ece3e6fb8d2b24370b5aaf
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2023-02-17 15:14:34 +0100

    Refactor test.py: cambiate modalità di raccolta dei test

--- src/test.py
+++ src/test.py
@@ -1,7 +1,11 @@
 from timeit import default_timer as timer
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import sys
+from src.graph import Graph
 from cc import cc
 from mst_kruskal import *
-import matplotlib.pyplot as plt
 
 
 def cc_test(g):
@@ -15,72 +19,72 @@ def mst_test(g):
     start = timer()
     mst = mst_kruskal(g)
     end = timer()
-    return [len(mst), round(end - start, 6)]
+    return [mst, round(end - start, 6)]
 
 
 def test():
-    cc_results = []
-    cc_results_time = []
-    mst_results = []
-    mst_results_time = []
-    cc_results_weighted = []
-    cc_results_time_weighted = []
-    mst_results_weighted = []
-    mst_results_time_weighted = []
-    n = 5  # numero iniziale di nodi 5
-    p = 10  # probabilità di archi iniziale 10
+    sizes = [2 ** i for i in range(0, 13)]  # array numero nodi nel grafo
+    probs = [20, 40, 60, 80, 100]  # array di probabilità in %
+    cc_data = {p: [] for p in probs}
+    mst_data = {p: [] for p in probs}
+    for s in sizes:
+        for p in probs:
+            file_name = "size=" + str(s) + "_p=" + str(p).replace(".", "") + ".dat"
+            g = Graph(s, True)
+            g.populate_adj(p)
 
-    for i in range(1, 10):  # dovrebbe essere 13
-        g = Graph(n)
-        g.populate_adj(p)
-        comp = cc_test(g)
-        cc_results.append(comp[0])
-        cc_results_time.append(comp[1])
-        mst = mst_test(g)
-        mst_results.append(mst[0])
-        mst_results_time.append(mst[1])
+            with open("data/in_" + file_name, "wb") as f:
+                pickle.dump(g, f)
 
-        g = Graph(n, True)
-        g.populate_adj(p)
-        comp = cc_test(g)
-        cc_results_weighted.append(comp[0])
-        cc_results_time_weighted.append(comp[1])
-        mst = mst_test(g)
-        mst_results_weighted.append(mst[0])
-        mst_results_time_weighted.append(mst[1])
-        p += 5
-        # n += int((n * i) / 4)
-        n += 5
+            cc = cc_test(g)
+            cc_data[p].append((s, cc[1], cc[0]))
 
-    x = np.arange(1, len(mst_results_time) + 1)*10
-    plot_1 = plt.figure(1)
-    plt.plot(x, mst_results)
-    plt.plot(x, mst_results_weighted)
-    plt.xlabel("Dimensione, probabilità")
-    plt.ylabel("Numero")
-    plt.legend(["Non pesato", "pesato"])
-    plt.title("Confronto esecuzione MST tra grafo pesato e non pesato")
+            if cc[0] == 1:
+                mst = mst_test(g)
+                mst_data[p].append((s, mst[1], mst[0]))
+            else:
+                mst_data[p].append((s, None, None))
 
-    plot_2 = plt.figure(2)
-    plt.plot(x, cc_results)
-    plt.plot(x, cc_results_weighted)
-    plt.xlabel("Dimensione, probabilità")
-    plt.ylabel("Componenti connesse")
-    plt.legend(["Non pesato", "Pesato"])
-    plt.title("Confronto esecuzione CC tra grafo pesato e non pesato")
+            with open("data/out_cc_" + file_name, "wb") as f:
+                pickle.dump(cc[0], f)
 
-    plot_3 = plt.figure(3)
-    plt.plot(x, cc_results_weighted)
-    plt.plot(x, mst_results_weighted)
-    plt.xlabel("Dimensione, probabilità")
-    plt.ylabel("Tempo in s")
-    plt.legend(["CC", "MST"])
-    plt.title("Confronto esecuzione MST e CC in grafo pesato")
+            if cc[0] == 1:
+                with open("data/out_mst_" + file_name, "wb") as f:
+                    pickle.dump(mst[0], f)
 
-    plot_1.savefig('img/cc_mst.png')
-    plot_2.savefig('img/cc.png')
-    plot_3.savefig('img/time.png')
-    plt.clf()
+    with open("data/cc_overall.dat", "wb") as f:
+        pickle.dump(cc_data, f)
+    with open("data/mst_overall.dat", "wb") as f:
+        pickle.dump(mst_data, f)
+
+    # parte di plot
+
+    for p in probs:
+        plt.clf()
+
+        # Size-time ccs
+        plt.plot([rec[0] for rec in cc_data[p]], [rec[1] for rec in cc_data[p]])
+        plt.xlabel("Size")
+        plt.ylabel("Time(s)")
+        plt.savefig("../img/cc/cc_time_p=" + str(p).replace(".", "") + ".png")
+
+        # Size-Number of ccs
+        plt.clf()
+
+        plt.plot([rec[0] for rec in cc_data[p]], [rec[2] for rec in cc_data[p]])
+        plt.xlabel("Size")
+        plt.ylabel("Componenti connesse")
+        plt.savefig("../img/cc/cc_number_p=" + str(p).replace(".", "") + ".png")
+
+        # Size-time Kruskal
+        plt.clf()
+
+        plt.plot([rec[0] for rec in mst_data[p] if rec[1] is not None],
+                 [rec[1] for rec in mst_data[p] if rec[1] is not None])
+        plt.xlabel("Size")
+        plt.ylabel("Time(s)")
+        plt.savefig("../img/mst/mst_time_p=" + str(p).replace(".", "") + ".png")
+
 
 
 def main():
